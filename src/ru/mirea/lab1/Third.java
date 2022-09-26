7675fd20b7a27195905ea5387dbbc4414d9f2ff4
stand <standrik1701@gmail.com>
2022-09-27 00:55:48 +0300

    Second.java added

---
+++ src/ru/mirea/lab1/Third.java
@@ -0,0 +1,9 @@
+package ru.mirea.lab1;
+
+public class Third {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; ++i){
+            System.out.print(String.format("%.4f",(double)1/i) + " ");
+        }
+    }
+}
